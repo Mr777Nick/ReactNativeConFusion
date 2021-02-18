cccb1cea1aa9e2ce3509a947fe4032f78f7c7b25
Mr777Nick <bagas_naufal96@yahoo.co.id>
2021-02-18 23:37:27 +0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList, View } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -65,13 +66,39 @@ class About extends Component {
         title: 'About Us'
     };
 
+
     render() {
-        return (
-            <ScrollView>
-                <History />
-                <RenderLeadership leaders={this.props.leaders.leaders} />
-            </ScrollView>
-        );
+        if (this.props.leaders.isLoading) {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title={"Corporate Leadership"}>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return (
+                <ScrollView>
+                    <History />
+                    <Card
+                        title={"Corporate Leadership"}>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return (
+                <ScrollView>
+                    <History />
+                    <RenderLeadership leaders={this.props.leaders.leaders} />
+                </ScrollView>
+            );
+        }
+
     }
 }
 
